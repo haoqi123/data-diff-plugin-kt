7fb1280715deb01755262084828b1a466b6f2dfc
haoqi <1148648445@qq.com>
2021-12-17 18:50:27 +0800

    add code

--- src/main/kotlin/com/github/haoqi123/datadiffpluginkt/action/DataGenerateSqlAction.kt
+++ src/main/kotlin/com/github/haoqi123/datadiffpluginkt/action/DataGenerateSqlAction.kt
@@ -1,10 +1,10 @@
-package com.github.haoqi123.datadiffpluginkt
+package com.github.haoqi123.datadiffpluginkt.action
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
 class DataGenerateSqlAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        // TODO: insert action logic here
+
     }
 }
