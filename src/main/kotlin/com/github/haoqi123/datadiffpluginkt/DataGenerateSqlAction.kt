d77854c83825019cc1bfdba8e6a6757cbb3ed88b
haoqi <1148648445@qq.com>
2021-12-17 11:52:38 +0800

    update properties

---
+++ src/main/kotlin/com/github/haoqi123/datadiffpluginkt/DataGenerateSqlAction.kt
@@ -0,0 +1,10 @@
+package com.github.haoqi123.datadiffpluginkt
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class DataGenerateSqlAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        // TODO: insert action logic here
+    }
+}
